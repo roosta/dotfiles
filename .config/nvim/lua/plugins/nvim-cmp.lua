83fec9dd56ef6a8eb91cfba06d3d6f027d018418
Daniel Berg <mail@roosta.sh>
2025-06-22 21:52:08 +0200

    nvim/snippets: update setup to luasnippets
    Which includes nvim-cmp settings, and some unrelated improvements.
    Lockfile is a complete update of all plugin to latest, not gonna patch
    it up.
    
    Motivation: ultisnip setup kept breaking and I've had sever performance
    issues on less powerful hardware when loading vim-snippets in its
    entirety. I'm sure there are ways to lazy load or similar, but I felt my
    snippet stack needed a refresh anyways.
    
    This commit also makes emmet unneeded, as friendly-snippets has
    everything there, while also supporting completion.

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -14,11 +14,12 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-omni",
-    "quangnguyen30192/cmp-nvim-ultisnips",
+    "saadparwaiz1/cmp_luasnip"
   },
   config = function()
     local cmp = require('cmp')
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    local luasnip = require("luasnip")
 
     local t = function(str)
       return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -31,151 +32,120 @@ return {
 
     cmp.setup({
       snippet = {
-        expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
       },
       window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
       mapping = {
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
         ['<C-e>'] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
-        -- ['<CR>'] = cmp.mapping.confirm {
-          --   behavior = cmp.ConfirmBehavior.Insert,
-          --   select = true,
-          -- },
-          ['<CR>'] = cmp.mapping({
-            i = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false })
-              else
-                fallback()
-              end
+        ['<CR>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({
+                select = true,
+              })
             end
-          }),
-          ["<Tab>"] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                cmp.complete()
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-              elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
-              if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-              else
-                fallback()
-              end
-            end
-          }),
-          ["<S-Tab>"] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-              else
-                cmp.complete()
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-              elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-              else
-                fallback()
-              end
-            end,
-            s = function(fallback)
-              if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-              else
-                fallback()
-              end
-            end
-          }),
-          ['<C-j>'] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end
-          }),
-          ['<C-k>'] = cmp.mapping({
-            c = function()
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
-              end
-            end,
-            i = function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end
-          }),
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'ultisnips' },
-          { name = 'render-markdown' },
-          { name = 'path' }
-        }, {
-          { name = 'buffer' },
-        })
-      })
+          else
+            fallback()
+          end
+        end),
 
-      -- Set configuration for specific filetype.
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-        }, {
-          { name = 'buffer' },
-        })
-      })
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
 
-      -- Use buffer source for `/`.
-      cmp.setup.cmdline('/', {
-        completion = { autocomplete = false },
-        sources = {
-          -- { name = 'buffer' }
-          { name = 'buffer', options = { keyword_pattern = [=[[^[:blank:]].*]=] } }
-        }
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ['<C-j>'] = cmp.mapping({
+          c = function()
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+            end
+          end,
+          i = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end
+        }),
+        ['<C-k>'] = cmp.mapping({
+          c = function()
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+            end
+          end,
+          i = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end
+        }),
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'ultisnips' },
+        { name = 'render-markdown' },
+        { name = 'path' }
+      }, {
+        { name = 'buffer' },
       })
+    })
 
-      -- Use cmdline & path source for ':'.
-      cmp.setup.cmdline(':', {
-        completion = { autocomplete = false },
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
+    -- Set configuration for specific filetype.
+    cmp.setup.filetype('gitcommit', {
+      sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+      }, {
+        { name = 'buffer' },
       })
+    })
+
+    -- Search completion
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+    -- Use cmdline & path source for ':'.
+    cmp.setup.cmdline(':', {
+      completion = { autocomplete = false },
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
   end,
-
 }
