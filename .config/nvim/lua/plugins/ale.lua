7f57bd4e6543812ea6b781c452ea83842d40e093
Daniel Berg <mail@roosta.sh>
2025-07-03 00:28:29 +0200

    nvim/ale: set handlebars to use html linters

--- .config/nvim/lua/plugins/ale.lua
+++ .config/nvim/lua/plugins/ale.lua
@@ -8,9 +8,10 @@ return {
   "dense-analysis/ale",
   config = function()
     vim.g.ale_disable_lsp = 1
+    vim.g.ale_linter_aliases = {handlebars = {"html"}}
     vim.g.ale_linters = {
       -- lua = {'lua_language_server'},
-      javascript = {'eslint'}
+      javascript = {'eslint'},
     }
   end
 }
