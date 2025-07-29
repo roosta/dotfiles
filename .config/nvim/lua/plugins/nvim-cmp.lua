6ea4099a942e6405ff1d3a9e933cae154d0ab6d9
Daniel Berg <mail@roosta.sh>
2025-07-29 14:41:34 +0200

    nvim/cmp: fix search and cmd completion

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -132,20 +132,20 @@ return {
     })
 
     -- Search completion
-    cmp.setup.cmdline({ '/', '?' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    })
+    -- cmp.setup.cmdline({ '/', '?' }, {
+    --   mapping = cmp.mapping.preset.cmdline(),
+    --   sources = {
+    --     { name = 'buffer' }
+    --   }
+    -- })
     -- Use cmdline & path source for ':'.
-    cmp.setup.cmdline(':', {
-      completion = { autocomplete = false },
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
+    -- cmp.setup.cmdline(':', {
+    --   completion = { autocomplete = false },
+    --   sources = cmp.config.sources({
+    --     { name = 'path' }
+    --   }, {
+    --     { name = 'cmdline' }
+    --   })
+    -- })
   end,
 }
