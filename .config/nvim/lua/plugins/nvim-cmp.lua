6a06637545719cdca8517478d0d80f1ab41e1b3f
Daniel Berg <mail@roosta.sh>
2025-08-08 14:33:42 +0200

    nvim/cmp: fix issue with cmd, search completion

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -131,21 +131,22 @@ return {
       })
     })
 
-    -- Search completion
-    -- cmp.setup.cmdline({ '/', '?' }, {
-    --   mapping = cmp.mapping.preset.cmdline(),
-    --   sources = {
-    --     { name = 'buffer' }
-    --   }
-    -- })
-    -- Use cmdline & path source for ':'.
-    -- cmp.setup.cmdline(':', {
-    --   completion = { autocomplete = false },
-    --   sources = cmp.config.sources({
-    --     { name = 'path' }
-    --   }, {
-    --     { name = 'cmdline' }
-    --   })
-    -- })
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+          { name = 'cmdline' }
+        }),
+      matching = { disallow_symbol_nonprefix_matching = false }
+    })
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
   end,
 }
