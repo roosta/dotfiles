d0356198d2d2631753859a3e5a64e762ca69b52e
Daniel Berg <mail@roosta.sh>
2025-08-19 22:52:33 +0200

    zsh/fzf: move fedit to scripts submodule
    Proof of concept, but plan on doing this with several other util
    functions using fzf.

--- .zsh.d/utils.zsh
+++ .zsh.d/utils.zsh
@@ -1,3 +1,4 @@
+#!/usr/bin/env zsh
 # ┌───────────────────────────────────────┐ #
 # │ ┏━┓┓━┓┳ ┳  ┳ ┓┏┓┓o┳  ┓━┓              │ #
 # │ ┏━┛┗━┓┃━┫  ┃ ┃ ┃ ┃┃  ┗━┓              │ #
@@ -8,12 +9,33 @@
 # ----------------------------------------- #
 
 # Helper fn to ensure binary presenense
-require_binary() {
-  local binary=$1
-  local message=${2:-"Required binary '$binary' is not installed"}
+# require_binary() {
+#   local binary=$1
+#   local message=${2:-"Required binary '$binary' is not installed"}
+#
+#   if ! command -v "$binary" &> /dev/null; then
+#     echo "Error: $message" >&2
+#     return 1
+#   fi
+#   return 0
+# }
 
-  if ! command -v "$binary" &> /dev/null; then
-    echo "Error: $message" >&2
+# Helper fn to ensure binary presenense
+require_binary() {
+  local missing=()
+  
+  for binary in "$@"; do
+    if ! command -v "$binary" &> /dev/null; then
+      missing+=("$binary")
+    fi
+  done
+  
+  if [[ ${#missing[@]} -gt 0 ]]; then
+    if [[ ${#missing[@]} -eq 1 ]]; then
+      echo "Warning: binary '${missing[1]}' is not installed, skipping config..." >&2
+    else
+      echo "Warning: binaries are not installed: ${missing[*]}, skipping config..." >&2
+    fi
     return 1
   fi
   return 0
