8c0e5134cb28dc8e2ab0fb7a3f7d65a6441e8a7a
Daniel Berg <mail@roosta.sh>
2026-02-02 15:01:33 +0100

    nvim/hardtime: fix mouse
    Thats why I initially enabled keys, cause hardtime kept complaining
    about the arrow keys when I tried using the wheel.

--- .config/nvim/lua/plugins/hardtime.nvim.lua
+++ .config/nvim/lua/plugins/hardtime.nvim.lua
@@ -9,11 +9,6 @@ return {
   lazy = false,
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
-    disabled_keys = {
-      ["<Up>"] = false,
-      ["<Down>"] = false,
-      ["<Left>"] = false,
-      ["<Right>"] = false,
-    },
+    disable_mouse = false,
   },
 }
