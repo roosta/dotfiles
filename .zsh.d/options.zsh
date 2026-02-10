bae83d270210191695a7a0f40a200d967fb50dd1
Daniel Berg <mail@roosta.sh>
2026-02-10 12:55:33 +0100

    zsh/opts: cleanup
    Went through the opts, and some are already set by default, others I
    prefer the default. Some were prompt related from way back. Add some
    missing comments as well, always good to document what each of these do.

--- .zsh.d/options.zsh
+++ .zsh.d/options.zsh
@@ -6,10 +6,7 @@
 bindkey -v
 
 # try to avoid the 'zsh: no matches found...'
-setopt nonomatch
-
-# report the status of backgrounds jobs immediately
-setopt notify
+# setopt NO_NOMATCH
 
 # preform cd if command matches a directory
 setopt autocd
@@ -18,17 +15,7 @@ setopt autocd
 setopt extendedglob
 
 # allow use of comments in interactive code
-setopt interactivecomments
-
-# don't nice background tasks
-setopt NO_BG_NICE
-
-# Don't send SIGHUP to background processes when the shell exits.
-setopt nohup
-
-# no beeps plz
-# unsetopt beep
-# setopt nobeep
+setopt INTERACTIVE_COMMENTS
 
 # allow functions to have local options
 setopt LOCAL_OPTIONS
@@ -36,17 +23,8 @@ setopt LOCAL_OPTIONS
 # allow functions to have local traps
 setopt LOCAL_TRAPS
 
-# whenever a command completion is attempted, make sure the entire command path
-# is hashed first.
-setopt hash_list_all
-
 # not just at the end
-setopt completeinword
-
-# use zsh style word splitting
-setopt noshwordsplit
-
-setopt PROMPT_SUBST
+setopt COMPLETE_IN_WORD
 
 # Try to correct the spelling of command
 setopt CORRECT
@@ -59,8 +37,9 @@ HISTFILE=~/.histfile
 HISTSIZE=500000
 SAVEHIST=500000
 
-# Whenever  the  user  enters  a  line with history expansion, don't execute the line directly; instead, perform history
-# expansion and reload the line into the editing buffer.
+# Whenever the user enters a line with history expansion, don't execute the
+# line directly; instead, perform history expansion and reload the line into
+# the editing buffer.
 setopt HIST_VERIFY
 
 # Save each command's beginning timestamp (in seconds since the epoch) and the duration  (in  seconds)  to  the  history
@@ -71,22 +50,23 @@ setopt EXTENDED_HISTORY
 # clean up history
 setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
-#setopt HIST_EXPIRE_DUPS_FIRST
 
 # adds history incrementally and share it across sessions
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 
-#  enable a built in help command
+# enable a built in help command
 autoload -U run-help
 autoload run-help-git
-autoload run-help-svn
-autoload run-help-svk
-#unalias run-help
 alias help=run-help
 
-# Autoload some useful utilities.
+# Batch rename files using patterns
+# zmv '(*).txt' '$1.md'  # rename all .txt to .md
 autoload -Uz zmv
+
+# Like xargs but native to zsh. Handles argument list limits gracefully.
 autoload -Uz zargs
-autoload -Uz vcs_info
+
+## Edit command in $EDITOR (norm vv)
 autoload -Uz edit-command-line
+
