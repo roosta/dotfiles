5cc499c52e60328fe626c3252c1a219cd0b51c42
Daniel Berg <mail@roosta.sh>
2026-04-11 12:30:50 +0200

    nvim/lsp: track nvim-lspconfig
    Been doing it for a while, just forgot to stage. I don't remember why
    anymore except that there was an issue removing this.

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,12 @@
+-- ┌┐┐┐ ┬o┌┌┐  ┬  ┐─┐┬─┐┌─┐┌─┐┌┐┐┬─┐o┌─┐
+-- ││││┌┘││││──│  └─┐│─┘│  │ ││││├─ ││ ┬
+-- ┆└┘└┘ ┆┘ ┆  ┆─┘──┘┆  └─┘┘─┘┆└┘┆  ┆┆─┘
+-- ─────────────────────────────────────────────────────────────────────────
+
+return {
+  "neovim/nvim-lspconfig",
+  -- config = function()
+  --   require('lspconfig').pyright.setup{}
+  --   vim.keymap.set('n', 'gl', vim.diagnostic.open_float, {desc = 'Show diagnostics under cursor'})
+  -- end
+}
