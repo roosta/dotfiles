0b8641e38616e1518f9113581837df2919cd0fd1
Daniel Berg <mail@roosta.sh>
2026-04-24 10:36:23 +0200

    nvim/plugins: replace ale with nvim-lint
    Not sure about this yet, the setup was a bit awkward, but possibly more
    secure

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,36 @@
+-- ┌┐┐┐ ┬o┌┌┐  ┬  o┌┐┐┌┐┐
+-- ││││┌┘││││──│  ││││ │
+-- ┆└┘└┘ ┆┘ ┆  ┆─┘┆┆└┘ ┆
+-- ─────────────────────────────────────────────────────────────────────────
+
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    local trusted_dirs = {
+      vim.env.HOME .. '/src',
+      vim.env.HOME .. '/notes',
+    }
+    local function in_trusted_dir()
+      local cwd = vim.fn.getcwd()
+      for _, dir in ipairs(trusted_dirs) do
+        if cwd:find(dir, 1, true) == 1 then
+          return true
+        end
+      end
+      return false
+    end
+    require('lint').linters_by_ft = {
+      markdown = { 'proselint' },
+      text = { 'proselint' },
+      astro = { 'biomejs' }
+    }
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave', 'BufWinEnter' }, {
+      callback = function()
+        if in_trusted_dir() then
+          require('lint').try_lint()
+        end
+      end,
+    })
+  end
+
+}
