b34236d532de6590aed44b8ed9a211768f9eaacd
Daniel Berg <mail@roosta.sh>
2026-05-15 11:37:57 +0200

    hypr/binds: fix missing media keys
    Forgot to port it over

--- .config/hypr/binds.lua
+++ .config/hypr/binds.lua
@@ -35,6 +35,16 @@ hl.bind(main_mod .. " + equal", hl.dsp.layout("togglesplit"))
 hl.bind(main_mod .. " + SHIFT + space", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(main_mod .. " + space", hl.dsp.layout("cyclenext"))
 
+-- Media keys
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"), { repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { repeating = true })
+hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true })
+
+-- Requires playerctl
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"),       { locked = true })
+
 local directions = {
   left = "left",
   right = "right",
