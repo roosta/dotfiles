a787ffe856d13d0be01011f18f4c3680c402f24c
Daniel Berg <mail@roosta.sh>
2026-05-15 10:09:12 +0200

    hypr/garkbit: port to lua

--- .config/hypr/monitors/monitors.lua
+++ .config/hypr/monitors/monitors.lua
@@ -4,11 +4,7 @@
 -- ┘ Define monitors used in configs
 
 local monitors = {
-  center = "DP-1",
-  left = "DP-2",
-  right = "HDMI-A-1",
-  top = "HDMI-A-3",
-  tv = "HDMI-A-2"
+  main = "DP-1",
 }
 
 return monitors
