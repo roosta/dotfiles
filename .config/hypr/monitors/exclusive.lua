4059e2a62ec9c6cdbe2b89a441172dd4ee1d49f5
Daniel Berg <mail@roosta.sh>
2026-07-22 21:59:55 +0200

    hypr/monitors: fix exclusive config

---
+++ .config/hypr/monitors/exclusive.lua
@@ -0,0 +1,47 @@
+-- ┬─┐┐ │┌─┐┬  ┬ ┐┐─┐o┐ ┬┬─┐  ┬  ┬─┐┐ ┬┌─┐┬ ┐┌┐┐
+-- ├─ ┌┼┘│  │  │ │└─┐││┌┘├─   │  │─┤└┌┘│ ││ │ │
+-- ┴─┘┆ └└─┘┆─┘┆─┘──┘┆└┘ ┴─┘  ┆─┘┘ ┆ ┆ ┘─┘┆─┘ ┆
+-- Exclusive TV layout
+
+local monitors = require("monitors/monitors")
+local utils = require("monitors/utils")
+local primary_monitor = monitors.center
+local vars = require("variables").vars
+
+hl.monitor({
+  output = monitors.center,
+  mode = "3840x2160@240",
+  position = "0x0",
+  scale = "2",
+  bitdepth = 10,
+  vrr = 3
+})
+
+
+utils.add_workspaces(primary_monitor, {1,2,3,4,5,6,7,8,9,10}, 1)
+
+hl.on("monitor.added", function(m)
+  if m.name == primary_monitor then
+    utils.collect_workspaces(primary_monitor)
+  end
+end)
+
+hl.window_rule({
+  match = { class = "firefox-media" },
+  workspace = 5
+})
+
+hl.monitor({
+  output = monitors.top,
+  disabled = true,
+})
+
+hl.monitor({
+  output = monitors.left,
+  disabled = true,
+})
+
+hl.monitor({
+  output = monitors.right,
+  disabled = true,
+})
