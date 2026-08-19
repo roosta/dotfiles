ead8e779c2c0c7952af290a59f91590a4ec75aff
Daniel Berg <mail@roosta.sh>
2026-08-19 15:10:01 +0200

    nvim/codecompanion: switch to openrouter

--- .config/nvim/lua/plugins/codecompanion.nvim.lua
+++ .config/nvim/lua/plugins/codecompanion.nvim.lua
@@ -2,19 +2,43 @@
 -- │  │ ││ │├─ │  │ │││││─┘│─┤│││││ ││││ ││││┌┘││││
 -- └─┘┘─┘┆─┘┴─┘└─┘┘─┘┘ ┆┆  ┘ ┆┆└┘┆┘─┘┆└┘o┆└┘└┘ ┆┘ ┆
 -- ─────────────────────────────────────────────────────────────────────────
- -- ✨ AI-powered coding, seamlessly in Neovim
+-- ✨ AI-powered coding, seamlessly in Neovim
 
 return {
   "olimorris/codecompanion.nvim",
   lazy = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
-    -- "ravitemer/codecompanion-history.nvim"
   },
   keys = {
     { "<leader>aa", ":CodeCompanionActions<CR>", silent = true },
   },
   opts = {
+
+    adapters = {
+      http = {
+        openrouter = function()
+          return require("codecompanion.adapters").extend("openrouter", {
+            env = {
+              api_key = "cmd:pass show openrouter.ai/api_key",
+            },
+          })
+        end,
+      },
+    },
+
+    interactions = {
+      chat = {
+        adapter = "openrouter",
+      },
+      inline = {
+        adapter = "openrouter",
+      },
+      cmd = {
+        adapter = "openrouter",
+      }
+    },
+
     display = {
       action_palette = {
         provider = "telescope",
@@ -24,29 +48,7 @@ return {
           layout = "horizontal",
           height = 0.5,
         }
-      }
-    },
-    extensions = {
-        -- history = {
-        --     enabled = true,
-        --     opts = {
-        --         expiration_days = 14,
-        --     }
-        -- }
-    },
-    interactions = {
-      chat = {
-        adapter = "anthropic",
-        model = "claude-opus-4.8"
       },
-      inline = {
-        adapter = "anthropic",
-        model = "claude-opus-4.8"
-      },
-      cmd = {
-        adapter = "anthropic",
-        model = "claude-opus-4.8"
-      }
     },
   }
 }
